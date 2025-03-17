00898fc6fff796e87d9889b52a414aadf6b1980c
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2025-03-17 09:50:09 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -25,20 +25,9 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1525203313").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Assaulter_Shiv'))
-    if 'DYNO' in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
-    
-    else:
-        ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',True))
-    if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
-    else:
-        URL = "http://{}/".format(FQDN)
+   
+    URL = "https://strem711-d509b1880a5f.herokuapp.com/"
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'BoB_Files1')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001844691460")).split()))   
